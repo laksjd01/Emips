36f277e8f12744245919c1e45874cfde594e42d6
laksjd01 <ramosgodgod@gmail.com>
2024-11-09 23:15:22 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+This simple JavaScript file ensures smooth scrolling for each page and image navigation.
+
+javascript
+// Smooth scrolling for anchor links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+`
